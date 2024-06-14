beb01398fed40035d9326dece3db5712f2f60c99
Andrei <ierdna100@yahoo.com>
2024-06-14 02:16:29 -0400

    Forgot to pass arguments, oops

--- src/discordServer/periodicMessages/MinecraftInfo.ts
+++ src/discordServer/periodicMessages/MinecraftInfo.ts
@@ -13,8 +13,7 @@ import { Logger } from "../../logging/Logger.js";
 export class PeriodicMessage_MinecraftInfo extends PeriodicMessageBase {
     protected messageType: PeriodicMessageType = PeriodicMessageType.minecraftInfo;
 
-    private fetchNewestDataCallbackID: number | undefined = undefined;
-    private updateMessageCallbackID: number | undefined = undefined;
+    private updateMessageCallbackID: NodeJS.Timeout | undefined = undefined;
 
     constructor() {
         super();
@@ -34,13 +33,8 @@ export class PeriodicMessage_MinecraftInfo extends PeriodicMessageBase {
         }
 
         WSServer.connections.forEach((e) => e.ws.send(JSON.stringify({ opcode: WebsocketOpcodes.globalData })));
-        // wtf is wrong with this? Why does it not think it returns a number?????
-        this.fetchNewestDataCallbackID = setTimeout(() => this.fetchNewestData, Application.instance.env.WSGlobalDataFreqMs) as unknown as number;
-        this.updateMessageCallbackID = setTimeout(() => this.updateMessage, Application.instance.env.WSGlobalDataFreqMs * 0.2, undefined);
-        console.log(this.fetchNewestDataCallbackID);
-        console.log(this.updateMessageCallbackID);
-        console.log(Application.instance.env.WSGlobalDataFreqMs * 0.2);
-        console.log(() => this.updateMessage);
+        setTimeout(() => this.fetchNewestData(), Application.instance.env.WSGlobalDataFreqMs);
+        this.updateMessageCallbackID = setTimeout(() => this.updateMessage(undefined), Application.instance.env.WSGlobalDataFreqMs * 0.2);
     }
 
     public async updateMessage(data: MinecraftServerInteraction.GlobalData | undefined): Promise<void> {
