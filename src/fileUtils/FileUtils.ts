7db15599e318c33c09787957b8726aac5eaee09b
Andrei <ierdna100@yahoo.com>
2024-06-15 03:51:35 -0400

    Removed fileUitls, apparently this already comes with NodeJs

--- src/fileUtils/FileUtils.ts
+++
@@ -1,9 +0,0 @@
-export class FileUtils {
-    public static formatDirName(filename: string): string {
-        if (filename.charAt(filename.length - 1) == "/" || filename.charAt(filename.length - 1) == "\\") {
-            return filename.substring(0, filename.length - 1);
-        }
-
-        return filename;
-    }
-}
