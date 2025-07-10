8b6468c2cd2ef6285d7721a251be80df046cb944
Andrei <ierdna100@yahoo.com>
2025-07-10 02:04:53 -0400

    What if we dont initialize it at all

--- src/Install.ts
+++ src/Install.ts
@@ -1,6 +1,4 @@
 import fs from "fs";
-import { EnvManager } from "./EnvManager.js";
-import { Logger } from "./logging/Logger.js";
 
 export default class Application {
     constructor() {
@@ -9,10 +7,6 @@ export default class Application {
         }
         if (!fs.existsSync("./env")) {
             fs.mkdirSync("./env");
-        }
-        const success = EnvManager.readAndParse();
-        if (!success.success) {
-            success.errors.forEach((e) => Logger.fatal(e));
         }
     }
 }
