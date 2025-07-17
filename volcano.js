e387a312ac436ffe2f8d9a0928a1670931645cfd
Andrei <ierdna100@yahoo.com>
2025-07-17 14:53:41 -0400

    Removed debug files

--- volcano.js
+++
@@ -1,9 +0,0 @@
-import WebSocket from "ws";
-
-const ws = new WebSocket("wss://localhost:7500");
-ws.on("open", () => console.log("open"));
-ws.on("message", (data) => killme(data));
-
-function killme(data) {
-    console.log(data);
-}
