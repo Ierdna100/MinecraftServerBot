80bf64b116085b3fd0a767cdfb2844b17bb60913
Andrei <ierdna100@yahoo.com>
2025-07-25 23:41:40 -0400

    Forgot to register event handlers

--- src/wss/InteractionHandlers.ts
+++ src/wss/InteractionHandlers.ts
@@ -17,5 +17,15 @@ export const websocketResponses: Partial<WSResMap> = {
     [WSOpcodes.M2D_PlayerJoinSuccess]: new WSPlayerJoinSuccessHandler(),
     [WSOpcodes.M2D_PlayerJoinFail_IPBanned]: new WSPlayerJoinFailIPBannedHandler(),
     [WSOpcodes.M2D_PlayerJoinFail_NotWhitelisted]: new WSPlayerJoinFailNotWhitelistedHandler(),
-    [WSOpcodes.M2D_PlayerJoinFail_UnregisteredIP]: new WSPlayerJoinFailUnregisteredIPHandler()
+    [WSOpcodes.M2D_PlayerJoinFail_UnregisteredIP]: new WSPlayerJoinFailUnregisteredIPHandler(),
+    [WSOpcodes.M2D_Message]: new WSPlayerJoinFailUnregisteredIPHandler(),
+    [WSOpcodes.M2D_Death]: new WSPlayerJoinFailUnregisteredIPHandler(),
+    [WSOpcodes.M2D_ServerStarted]: new WSPlayerJoinFailUnregisteredIPHandler(),
+    [WSOpcodes.M2D_ServerStopping]: new WSPlayerJoinFailUnregisteredIPHandler(),
+    [WSOpcodes.M2D_ServerStopped]: new WSPlayerJoinFailUnregisteredIPHandler(),
+    [WSOpcodes.M2D_PlayerLeft]: new WSPlayerJoinFailUnregisteredIPHandler(),
+    [WSOpcodes.M2D_PlayerJoin]: new WSPlayerJoinFailUnregisteredIPHandler(),
+    [WSOpcodes.M2D_Advancement]: new WSPlayerJoinFailUnregisteredIPHandler(),
+    [WSOpcodes.M2D_ServerInfo]: new WSPlayerJoinFailUnregisteredIPHandler(),
+    [WSOpcodes.M2D_SavedGame]: new WSPlayerJoinFailUnregisteredIPHandler()
 };
