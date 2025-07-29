4b245f48e0492c575e6ed629476580426bf8d9e3
Andrei <ierdna100@yahoo.com>
2025-07-29 19:17:39 -0400

    Added logging to language formatter

--- src/LanguageManager.ts
+++ src/LanguageManager.ts
@@ -1,31 +1,41 @@
 import fs from "fs";
 import { formatStringMinecraft } from "./dto/Text.js";
+import { Logger } from "./logging/Logger.js";
 
 export default class LanguageManager {
     private static en_us: Record<string, string>;
 
     public static initialize() {
         this.en_us = JSON.parse(fs.readFileSync("./data/minecraftStringKeys.json").toString());
+        const loaded = Object.keys(this.en_us).length;
+        Logger.info(`Language manager loaded ${loaded} keys`);
     }
 
     public static fromKey(key: string): string | undefined {
+        Logger.detail(`fromKey(${key}) returned '${this.en_us[key]}'`);
         return this.en_us[key];
     }
 
     public static formatterFromKey(key: string) {
+        Logger.detail(`formatterFromKey(${key}) returned '${this.en_us[key]}'`);
         return this.en_us[key];
     }
 
     public static stringFromKey(key: string, ...values: string[]) {
-        return formatStringMinecraft(this.en_us[key], ...values);
+        const formatted = formatStringMinecraft(this.en_us[key], ...values);
+        Logger.detail(`stringFromKey(${key}, ${values.toString()}) returned '${formatted}'`);
+        return formatted;
     }
 
     public static entityStringFromPartial(key: string) {
+        let ret;
         if (key.startsWith("minecraft:")) {
             key = key.replace("minecraft:", "");
-            return `minecraft.entity.${key}`;
+            ret = `minecraft.entity.${key}`;
         } else {
-            return `minecraft.entity.${key}`;
+            ret = `minecraft.entity.${key}`;
         }
+        Logger.detail(`entityStringFromPartial(${key}) returned '${ret}'`);
+        return ret;
     }
 }
