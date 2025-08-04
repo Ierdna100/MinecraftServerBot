e732bceddda9fb14efebeb8b934253c39b26cdf5
Andrei <ierdna100@yahoo.com>
2025-08-03 20:12:55 -0400

    Forgot to add commands to command registry

--- src/discord/InteractionHandlers.ts
+++ src/discord/InteractionHandlers.ts
@@ -1,6 +1,8 @@
 import { ButtonFactory_AcceptAuthentication, ButtonFactory_RejectAuthentication } from "./buttons/AuthenticationResponse.js";
 import ButtonFactoryBase from "./buttons/ButtonBase.js";
 import { ButtonFactory_ConfirmIP, ButtonFactory_DenyIP } from "./buttons/UnregisteredIPResponse.js";
+import { SlashCommand_Administrators } from "./commands/Administrators.js";
+import { SlashCommand_ForceBackup } from "./commands/ForceBackup.js";
 import SlashCommand_Info from "./commands/Info.js";
 import SlashCommand_RequestAuthentication from "./commands/RequestAuthentication.js";
 import SlashCommandBase from "./commands/SlashCommandBase.js";
@@ -8,7 +10,9 @@ import SlashCommandBase from "./commands/SlashCommandBase.js";
 // prettier-ignore
 export const slashComamnds: SlashCommandBase[] = [
     new SlashCommand_RequestAuthentication(),
-    new SlashCommand_Info()
+    new SlashCommand_Info(),
+    new SlashCommand_Administrators(),
+    new SlashCommand_ForceBackup()
 ];
 
 // prettier-ignore
